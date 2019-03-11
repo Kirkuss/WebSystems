8fa2a6e2bca6b3500aac3798982808009b2fd898
Enrique Valverde Soriano <43791767+Kirkuss@users.noreply.github.com>
2019-03-11 11:25:59 +0100

    First approach to notes app

---
+++ notes/app/controllers/notes_controller.rb
@@ -0,0 +1,50 @@
+class NotesController < ApplicationController
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def new
+    # Se crea siempre una nueva nota para evitar retornos nulos, los campos de error se generan en un div de la clase field_with_errors y se pueden definir en css usando dicha clase.
+    @note = Note.new
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def create
+    @note = Note.new(note_params)
+
+    if @note.save
+      redirect_to @note
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @note = Note.find(params[:id])
+
+    if @note.update(note_params)
+      redirect_to @note
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
+
+    redirect_to notes_path
+  end
+
+  private
+    def note_params
+      params.require(:note).permit(:title, :text)
+    end
+end
